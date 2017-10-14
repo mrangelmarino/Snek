2c3db6a4212306fab7eb61337c43394ebca9f0b2
Angel Marino <me@angelmarino.com>
2017-10-14 18:23:59 -0400

    set up project structure

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict'
+
+class Snek {
+  constructor() {
+  }
+
+  canvas() {
+    // draw canvas
+  }
+
+  goal() {
+    // set game goal for snake
+  }
+
+  gameOver() {
+    // determine if player lost
+  }
+
+  step() {
+    // increment game
+  }
+
+  init() {
+    // initialize game
+  }
+
+}
+
+// surface constructor in absence of import or require
+window.Snek = window.Snek || Snek
