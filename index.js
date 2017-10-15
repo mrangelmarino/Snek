244d71ea01564aee49cee7c2cfddd4a86c82703b
Angel Marino <me@angelmarino.com>
2017-10-14 22:58:48 -0400

    snake movement

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ class Snek {
 
     this.element = document.querySelectorAll(element)
     this.options = Object.assign(defaults, options)
-    this.snake = []
     this.goal = {}
     this.direction = 'right'
+
+    this.canvas()
     this.init()
   }
 
@@ -27,13 +28,40 @@ class Snek {
     target.appendChild(canvas)
   }
 
-  placeGoal() {
-    // set game goal for snake
+  setSnake() {
+    this.snake = []
+
+    for(var i = 4; i >= 0; i--) {
+      this.snake.push({ x: i, y: 0 })
+    }
+    console.log('initial snake', this.snake)
+  }
+
+  placeSnake() {
     const ctx = this.options.ctx
+    const snake = this.snake
     const size = this.options.size
 
+    ctx.clearRect(0, 0, this.options.canvasWidth, this.options.canvasHeight)
+
+    ctx.fillStyle = 'green'
+    ctx.strokeStyle = 'white'
+
+    for(let i = 0; snake.length > i; i++) {
+      ctx.fillRect(snake[i].x * size, snake[i].y * size, size, size)
+      ctx.strokeRect(snake[i].x * size, snake[i].y * size, size, size)
+    }
+  }
+
+  setGoal() {
+    // set game goal for snake
     this.goal.x = this.placePoint()
     this.goal.y = this.placePoint()
+  }
+
+  placeGoal() {
+    const ctx = this.options.ctx
+    const size = this.options.size
 
     ctx.fillStyle = 'orange'
     ctx.fillRect(this.goal.x, this.goal.y, size, size)
@@ -45,17 +73,56 @@ class Snek {
     return Math.floor(Math.random() * (max-min)) + min
   }
 
-  gameOver() {
+  gameOver(x, y) {
     // determine if player lost
   }
 
   step() {
-    // increment game
+    const snake = this.snake
+    console.log(snake)
+    let x = snake[0].x
+    let y = snake[0].y
+
+    switch(this.direction) {
+      case 'right':
+        x++
+        break;
+      case 'down':
+        y++
+        break;
+      case 'left':
+        x--
+        break;
+      case 'up':
+        y--
+        break;
+    }
+
+    this.gameOver(x, y)
+
+    const tail = snake.pop()
+    tail.x = x
+    tail.y = y
+    snake.unshift(tail)
+
+    this.snake = snake
+
+    this.placeSnake()
+    this.placeGoal()
+
+  }
+
+  start() {
+    clearInterval(this.interval)
+    this.interval = setInterval(this.step.bind(this), 1000)
   }
 
   init() {
-    this.canvas()
+    this.setSnake()
+    this.placeSnake()
+    this.setGoal()
     this.placeGoal()
+    this.start()
   }
 
 }
